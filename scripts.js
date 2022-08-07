b01227b7bff8ac7a6748eb85f2394c6a5630a267
Natalyane <natvitorino@pop-os.localdomain>
2022-08-07 13:28:42 -0400

    criei a função para riscar as tarefas realizadas

---
+++ scripts.js
@@ -0,0 +1,59 @@
+const inputElement = document.querySelector(".new-task-input");
+const addTaskButton = document.querySelector('.new-task-button')
+
+const tasksContainer = document.querySelector(".tasks-container");
+
+const validateInput = () => inputElement.value.trim().length > 0;
+
+const handleAddTask = () => {
+    const inputIsValid = validateInput();
+
+    console.log(inputIsValid);
+
+    if (!inputIsValid) {
+        return inputElement.classList.add("error");
+    }
+
+    const taskItemContainer = document.createElement('div')
+    taskItemContainer.classList.add('task-item')
+
+    const taskContent = document.createElement('p')
+    taskConten .innerText = inputElement.value;
+
+    taskContent.addEventListener('click', () => handleClick(taskContent))
+
+    const deleteItem = document.createElement("p");
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("fa-trash-alt");
+
+    deleteItem.addEventListener('click', () => handleDeleteClick())
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+
+    inputElement.value = "";
+};
+
+const handleClick = (taskContent) => {
+    const tasks = tasksContainer.childNodes;
+};
+    for (const task of tasks) {
+        if (task.firstChild,isSameNode( taskContent)) {
+            task.firstChild.classList.toggle("completed");
+        }
+    }
+
+
+const handleInputChange = () => {
+    const inputIsValid = validateInput();
+ 
+    if (inputIsValid) {
+        return inputElement.classList.remove("error")
+    }
+};
+
+addTaskButton.addEventListener("click", () => handleAddTask())
+
+inputElement.addEventListener("change", () => handleInputChange());
