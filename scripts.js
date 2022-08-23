d791107b933fd5722020cb5897da8d842b6692a2
Natalyane <natvitorino@pop-os.localdomain>
2022-08-22 21:27:29 -0400

    código do to do list completo

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const inputElement = document.querySelector(".new-task-input");
-const addTaskButton = document.querySelector('.new-task-button')
+const addTaskButton = document.querySelector(".new-task-button");
 
 const tasksContainer = document.querySelector(".tasks-container");
 
@@ -12,48 +12,118 @@ const handleAddTask = () => {
 
     if (!inputIsValid) {
         return inputElement.classList.add("error");
-    }
+    } 
 
-    const taskItemContainer = document.createElement('div')
-    taskItemContainer.classList.add('task-item')
+    const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
 
-    const taskContent = document.createElement('p')
-    taskConten .innerText = inputElement.value;
+    const taskContent = document.createElement("p");
+    taskConten.innerText = inputElement.value;
 
-    taskContent.addEventListener('click', () => handleClick(taskContent))
+    taskContent.addEventListener("click", () => handleClick(taskContent));
 
-    const deleteItem = document.createElement("p");
+    const deleteItem = document.createElement("i");
     deleteItem.classList.add("far");
     deleteItem.classList.add("fa-trash-alt");
 
-    deleteItem.addEventListener('click', () => handleDeleteClick())
+    deleteItem.addEventListener("click", () => 
+        handleDeleteClick(taskItemContainer, taskContent)
+    );
 
     taskItemContainer.appendChild(taskContent);
     taskItemContainer.appendChild(deleteItem);
-
+   
     tasksContainer.appendChild(taskItemContainer);
 
     inputElement.value = "";
+
+    updateLocalStorage();
 };
 
 const handleClick = (taskContent) => {
     const tasks = tasksContainer.childNodes;
-};
+
     for (const task of tasks) {
-        if (task.firstChild,isSameNode( taskContent)) {
+      const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
+
+        if (currentTaskIsBeingClicked) {
             task.firstChild.classList.toggle("completed");
         }
     }
 
+    updateLocalStorage();
+};
+
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+    const tasks = tasksContainer.childNodes;
+ 
+    for (const task of tasks) {
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode (taskContent);
+        
+        if (currentTaskIsBeingClicked) {
+        taskItemContainer.remove();
+    }
+ }
+
+    updateLocalStorage();
+};
 
 const handleInputChange = () => {
     const inputIsValid = validateInput();
- 
-    if (inputIsValid) {
+
+    if (inputIsValid) { 
         return inputElement.classList.remove("error")
-    }
+     }
 };
 
-addTaskButton.addEventListener("click", () => handleAddTask())
+const updateLocalStorage = () => {
+    const tasks = tasksContainer.childNodes;
 
-inputElement.addEventListener("change", () => handleInputChange());
+    const localStorageTasks = [...tasks].map((task) => {
+        const content = task.firstChild;
+        const isCompleted = content.classList.contains("completed");
+
+        return { description: content.innerText, isCompleted };
+    });
+
+    localStorage.setItem("tasks", JSON.stringify( localStorageTasks));
+};
+
+const refleshTaskUsingLocalStorage = () => {
+   const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+   
+   if (!tasksFromLocalStorage) return;
+
+   for (const task of tasksFromLocalStorage) {
+    const taskItemContainer = document.createElement("p");
+    taskItemContainer.classList.add("task-item");
+
+    const taskContent = document.createElement("p");
+    taskContent.innerText = task.description;
+
+    if(task.isCompleted) {
+        taskContent.classList.add("completed");
+    }
+
+    taskContent.addEventListener("click", () => handleClick(taskContent)); 
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("fa-trash-alt");
+
+    deleteItem.addEventListener("click", () =>
+        handleDeleteClick(taskItemContainer, taskContent)
+    );
+   
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+   }
+};
+
+refreshTasksUsingLocalStorage();
+
+addTaskButton.addEventListener("click", () => handleAddTask());
+
+inputElement.addEventListener("change",() => handleInputChange());
